4427b16bebdfedd058f01ba3caf52e8e269dfd49
github username <github email address>
2017-05-16 14:15:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 function getFirstSelector(selector) {
-return document.querySelector(selector)
+  return document.querySelector(selector)
 }
 
 function nestedTarget() {
-return document.querySelector('#nested .target')
-}
-
-function deepestChild() {
-  return document.querySelector('#grand-node div div div div')
+  return document.querySelector("#nested .target")
 }
 
 function increaseRankBy(n) {
-  var list = document.querySelectorAll('.ranked-list')
-  for (let i = 0, l = list.length; i < l; i++) {
-    let children = list[i].children
-  for (let j = 0, k = children.length; j < k; j++) {
-    children[j].innerHTML =  parseInt(children[j].innerHTML, 10) + n
+  var rank = document.querySelectorAll(".ranked-list li")
+  for (var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (parseInt(rank[i].innerHTML, 10) + n).toString()
   }
- }
+}
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div")
 }
